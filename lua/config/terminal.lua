5fc0fcc9616f647940cfb6158d6c2fbb052c4860
samu698 <gpg@cerea.dev>
2024-11-11 21:39:17 +0100

    This is the buffer

---
+++ lua/config/terminal.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+M.toggleterm = {
+    open_mapping = require("config.mappings").terminal_open,
+    autochdir = true,
+    direction = 'float',
+    float_opts = {
+        border = 'curved'
+    }
+}
+
+M.flatten = {
+    window = {
+        open = "current",
+    },
+    post_open = function(bufnr, winnr, ft, is_blocking)
+        vim.print("Post open")
+        if ft == "gitcommit" or ft == "gitrebase" then
+            vim.api.nvim_create_autocmd("BufWritePost", {
+                buffer = bufnr,
+                once = true,
+                callback = vim.schedule_wrap(function()
+                    vim.print("Delete buffer")
+                    vim.api.nvim_buf_delete(bufnr, {})
+                end)
+            })
+        end
+    end,
+}
+
+return M
