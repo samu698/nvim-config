4d89adfe34230175a873efa559fd4623886e055c
samu698 <gpg@cerea.dev>
2024-11-11 21:59:03 +0100

    Hello

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -13,17 +13,19 @@ M.flatten = {
     window = {
         open = "current",
     },
-    post_open = function(bufnr, winnr, ft, is_blocking)
-        if ft == "gitcommit" or ft == "gitrebase" then
-            vim.api.nvim_create_autocmd("BufWritePost", {
-                buffer = bufnr,
-                once = true,
-                callback = vim.schedule_wrap(function()
-                    vim.api.nvim_buf_delete(bufnr, {})
-                end)
-            })
-        end
-    end,
+    callbacks = {
+        post_open = function(bufnr, winnr, ft, is_blocking)
+            if ft == "gitcommit" or ft == "gitrebase" then
+                vim.api.nvim_create_autocmd("BufWritePost", {
+                    buffer = bufnr,
+                    once = true,
+                    callback = vim.schedule_wrap(function()
+                        vim.api.nvim_buf_delete(bufnr, {})
+                    end)
+                })
+            end
+        end,
+    }
 }
 
 return M
