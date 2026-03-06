e82e1e705daa56502425d2ae17d3cf6753867deb
samu698 <gpg@cerea.dev>
2026-03-06 14:51:05 +0100

    Some bugs to iron out

---
+++ lua/functions/preview.lua
@@ -0,0 +1,177 @@
+local fn = {}
+
+local Popup = require("nui.popup")
+local ns = vim.api.nvim_create_namespace("ui_preview")
+
+---@class Preview.show_item_opts
+---@field text string? The text to show in the border
+---@field full_open boolean?
+
+---@class Preview.bufinfo
+---@field id integer
+---@field full boolean
+
+---@class Preview: NuiPopup
+---@field private buffers table<string, Preview.bufinfo>
+---@field private current_buf integer?
+---@field private to_show { item: Quickfix.item, bufnr: integer }?
+---@diagnostic disable-next-line: undefined-field
+local Preview = Popup:extend("Preview")
+
+--- Create a preview window
+--- @param opts nui_popup_options
+function Preview:init(opts)
+    ---@diagnostic disable-next-line: undefined-field
+    Preview.super.init(self, opts)
+
+    self.buffers = {}
+    -- FIXME: make sure to create the border text
+end
+
+--- Show an item in the preview
+---@param item Quickfix.item The item to show
+---@param opts Preview.show_item_opts
+function Preview:show_item(item, opts)
+    local bufnr = self:get_or_open_buffer(item.path, opts.full_open or false)
+    local text = opts.text or item.path
+    -- Set the border text
+    self.border:set_text("top", text, "right")
+
+    if not self.winid then
+        -- Schedule the items to be showed, if the window is not displayed
+        self.to_show = { item = item, bufnr = bufnr }
+    else
+        -- Show the new buffer on the window
+        self:update_window(item, bufnr)
+    end
+end
+
+---@param item Quickfix.item
+---@param bufnr integer
+---@private
+function Preview:update_window(item, bufnr)
+    if bufnr ~= self.current_buf then
+        if self.current_buf then
+            vim.api.nvim_buf_clear_namespace(self.current_buf, ns, 0, -1)
+        end
+
+        vim.api.nvim_win_set_buf(self.winid, bufnr)
+        vim.wo[self.winid].number = true
+        self.current_buf = bufnr
+    end
+
+    -- Move the currsor to the correct line and highlight it
+    vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+    if item.line then
+        vim.api.nvim_win_set_cursor(self.winid, { item.line, 0 } )
+        vim.hl.range(bufnr, ns, "Search", { item.line - 1, 0 }, { item.line - 1, -1 })
+    end
+end
+
+function Preview:_open_window()
+    ---@diagnostic disable-next-line: undefined-field
+    Preview.super._open_window(self)
+
+    if self.to_show then
+        self:update_window(self.to_show.item, self.to_show.bufnr)
+        self.to_show = nil
+    end
+end
+
+--- Removes loaded buffers from the cache, except the currenty shown one
+function Preview:clear_buffers()
+    if not self.buffers then return end
+
+    local current_path = nil
+    local current_bufinfo = nil
+    for path, bufinfo in pairs(self.buffers) do
+        if bufinfo.id ~= self.current_buf then
+            vim.api.nvim_buf_delete(bufinfo.id, {})
+        else
+            current_path = path
+            current_bufinfo = bufinfo
+        end
+    end
+
+    if current_path and current_bufinfo then
+        self.buffers = { [current_path] = current_bufinfo }
+    end
+end
+
+function Preview:unmount()
+    ---@diagnostic disable-next-line: undefined-field
+    Preview.super.unmount(self)
+
+
+    if self.current_buf then
+        vim.api.nvim_buf_clear_namespace(self.current_buf, ns, 0, -1)
+        self.current_buf = nil
+    end
+    Preview:clear_buffers()
+end
+
+---@param path string The path of the buffer to open
+---@param full boolean Whether it is a full buffer
+---@return integer bufnr
+---@private
+function Preview:get_or_open_buffer(path, full)
+    local bufinfo = self.buffers[path] ---@type Preview.bufinfo?
+    if bufinfo and (not full or bufinfo.full) then
+        return bufinfo.id
+    end
+
+    -- Use an existing buffer if it is present
+    local existing = vim.fn.bufnr(path)
+    if existing ~= -1 then
+        return existing
+    end
+
+    if full then
+        if bufinfo then vim.api.nvim_buf_delete(bufinfo.id, {}) end
+        bufinfo = fn.load_buffer_full(path)
+    else
+        bufinfo = fn.load_buffer_lines(path)
+    end
+
+    self.buffers[path] = bufinfo
+    return bufinfo.id
+end
+
+--- Create a preview buffer by copying the lines of the file
+---@param path string
+---@return Preview.bufinfo bufinfo
+function fn.load_buffer_lines(path)
+    -- TODO: Error checking
+    local lines = vim.fn.readfile(path)
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+
+    local ft = vim.filetype.match({ filename = path }) or ""
+    vim.bo[bufnr].modifiable = false
+    vim.bo[bufnr].filetype = ft
+
+    return {
+        id = bufnr,
+        full = false,
+    }
+end
+
+--- Open a preview buffer by fully loading the file
+---@param path string
+---@return Preview.bufinfo bufinfo
+function fn.load_buffer_full(path)
+    local bufnr = vim.fn.bufadd(path)
+    vim.fn.bufload(bufnr)
+    vim.bo[bufnr].modifiable = false
+
+    return {
+        id = bufnr,
+        full = true,
+    }
+end
+
+---@alias Preview.constructor fun(opts: nui_popup_options): Preview
+---@type Preview|Preview.constructor
+local CtorPreview = Preview
+
+return CtorPreview
--- lua/functions/quickfix.lua
+++ lua/functions/quickfix.lua
@@ -1,195 +1,182 @@
 local M = {}
-
----@diagnostic disable: unused-function
-
 -- Function to replace the quickfix window, using a nui.nvim menu instead
 
---[[
-        bufnr	buffer number; must be the number of a valid
-            buffer
-        filename	name of a file; only used when "bufnr" is not
-            present or it is invalid.
-        module	name of a module; if given it will be used in
-            quickfix error window instead of the filename.
-        lnum	line number in the file
-        end_lnum	end of lines, if the item spans multiple lines
-        pattern	search pattern used to locate the error
-        col		column number
-        vcol	when non-zero: "col" is visual column
-            when zero: "col" is byte index
-        end_col	end column, if the item spans multiple columns
-        nr		error number
-        text	description of the error
-        type	single-character error type, 'E', 'W', etc.
-        valid	recognized error message
-        user_data
-            custom data associated with the item, can be
-            any type.
---]]
-
--- Implementation:
--- Users call open quickfix
---  -> the quickfix opens
---  -> the previous quickfix gets deleted (TODO: quickfix nesting)
---  -> actions get handled by 
-
----@class Quickfix
----@field preview_buffers table<string, integer>
----@field ui Quickfix.ui
----
----@class Quickfix.ui
----@field menu NuiMenu
----@field preview NuiPopup
----@field outer NuiLayout
+local Popup = require("nui.popup")
+local Menu = require("nui.menu")
+local Preview = require('functions.preview')
+local Layout = require("nui.layout")
 
 ---@class Quickfix.item
 ---@field text string Short description of the quickfix item
 ---@field path string The path where to find the quickfix location
----@field pos Quickfix.item.pos?
----
----@class Quickfix.item.pos
----@field line number
----@field col number?
+---@field line integer? The line of the item
 
----@type Quickfix?
-local quickfix = nil
+---@type NuiMenu?
+local menu = nil
+
+---@type integer?
+local longest_line = nil
+
+---@type Preview?
+local preview = nil
+
+---@type NuiLayout?
+local layout = nil
+
+---Opens a new quickfix
+---@param title string
+---@param items Quickfix.item[]
+function M.open(title, items)
+    if layout then M.close() end
+    if #items == 0 then return end
+
+    if #items > 1 then
+        M.create_menu(title, items)
+    end
+
+    local show_item = nil
+    if #items == 1 then
+        show_item = items[1]
+    end
+
+    M.create_ui(show_item)
+    M.update_layout()
+
+    assert(layout)
+    layout:mount()
+end
+
+---Opens a new quickfix, uses qflist entries as input
+---@param title string
+---@param entires Quickfix.item[]
+function M.open_qflist(title, entires)
+    entires = M.conv_qlist_entries(entires)
+    M.open(title, entires)
+end
+
+---Closes the currently open quickfix
+function M.close()
+    if layout then
+        layout:unmount()
+    end
+
+    menu = nil
+    longest_line = nil
+    preview = nil
+    layout = nil
+end
+
+---@private
+---@param item Quickfix.item? The item to show in the preview
+function M.create_ui(item)
+    ---@type Preview
+    preview = Preview(M.popup_opts("", "right"))
+
+    if item then
+        preview:show_item(item, { full_open = true })
+    end
+
+    ---@type nui_layout_options
+    local layout_opts = {
+        position = "50%",
+        size = {
+            width = "90%",
+            height = "80%",
+        },
+    }
+
+    local box = Layout.Box({
+        Layout.Box(preview, { size = "100%" }),
+    }, { dir = "row", size = "100%" })
+
+    layout = Layout(layout_opts, box)
+end
+
+---@private
+function M.update_layout()
+    if not layout then return end
+    assert(preview)
+
+    local box
+    if menu then
+        box = Layout.Box({
+            Layout.Box(menu, { size = "20%" }),
+            Layout.Box(preview, { size = "80%" }),
+        }, { dir = "row", size = "100%" })
+    else
+        box = Layout.Box({
+            Layout.Box(preview, { size = "100%" }),
+        }, { dir = "row", size = "100%" })
+    end
+
+    layout:update(box)
+end
+
+---Create the menu component
+---@param title string
+---@param items Quickfix.item[]
+---@private
+function M.create_menu(title, items)
+    local lines = {}
+    longest_line = 0
+    for _, item in pairs(items) do
+        longest_line = math.max(longest_line, #item.text)
+        lines[#lines+1] = Menu.item(item.text, item)
+    end
+
+    local popup_opts = M.popup_opts(title, "center")
+
+    ---@type nui_menu_options
+    local menu_opts = {
+        lines = lines,
+        on_change = M.on_change,
+        on_submit = M.on_submit,
+        on_close = M.close,
+    }
+
+    menu = Menu(popup_opts, menu_opts)
+end
+
+---@param item Quickfix.item
+---@param _ NuiMenu
+---@private
+function M.on_change(item, _)
+    if not preview then return end
+    preview:show_item(item, {})
+end
+
+---@param item Quickfix.item
+---@private
+function M.on_submit(item)
+    if not preview then return end
+
+    menu = nil
+    preview:show_item(item, { full_open = true })
+    M.update_layout()
+end
 
 ---@param text string
 ---@param align nui_t_text_align
 ---@return nui_popup_options
-local function popup_config(text, align)
-    return {
+---@private
+function M.popup_opts(text, align)
+    ---@type nui_popup_options
+    local opts = {
         border = {
-            style = "single",
+            style = "rounded",
             text = {
                 top = text,
                 top_align = align,
             },
-        }
-    }
-end
-
---- Load a file preview buffer
----@param path string
----@return integer? bufnr
-local function load_preview(path)
-    -- TODO: Error checking
-    local lines = vim.fn.readfile(path)
-    local bufnr = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-
-    local ft = vim.filetype.match({ filename = path }) or ""
-    vim.bo[bufnr].modifiable = false
-    vim.bo[bufnr].filetype = ft
-    return bufnr
-end
-
---- Remove the currently open quickfix
-local function close()
-    -- TODO
-end
-
----@param item NuiTree.Node
-local function on_change(item, _)
-    ---@cast item +Quickfix.item
-    if not quickfix then return end
-
-    ---@type integer?
-    local bufnr = quickfix.preview_buffers[item.path]
-    if not bufnr then
-        bufnr = load_preview(item.path)
-        if not bufnr then return end
-        quickfix.preview_buffers[item.path] = bufnr
-    end
-
-    local preview = quickfix.ui.preview
-    preview.border:set_text("top", item.text, "right")
-    vim.api.nvim_win_set_buf(preview.winid, bufnr)
-    if item.pos then
-        local line = item.pos.line
-        local col = item.pos.col or 0
-        vim.api.nvim_win_set_cursor(preview.winid, { line, col } )
-    end
-    vim.wo[preview.winid].number = true
-end
-
----@param item NuiTree.Node
-local function on_submit(item)
-    ---@cast item +Quickfix.item
-    if not quickfix then return end
-    -- TODO
-end
-
---- Generate the UI for the quickfix window
----@param title string The title to give to the quickfix
----@param items Quickfix.item[] list of items to show in the quickfix
----@return Quickfix.ui
-local function make_ui(title, items)
-    local Layout = require("nui.layout")
-    local Menu = require("nui.menu")
-    local Popup = require("nui.popup")
-
-    local menu_items = {}
-    local longest_line = 0
-    for _, item in ipairs(items) do
-        longest_line = math.max(longest_line, #item.text)
-        local menu_item = Menu.item(item.text, item)
-        menu_items[#menu_items+1] = menu_item
-    end
-
-    local menu = Menu(
-        popup_config(title, "center"),
-        {
-            lines = menu_items,
-            -- TODO: try to remove schedule_wrap
-            -- This is _probably_ needed because the preview window is not 
-            -- present when on_change is called
-            on_change = vim.schedule_wrap(on_change),
-            on_submit = vim.schedule_wrap(on_submit),
-        }
-    )
-    local preview = Popup(popup_config("", "right"))
-
-    local columns = vim.o.columns
-    local layout_width = math.ceil(columns * 0.9)
-    local menu_width = math.min(longest_line + 3, math.floor(layout_width * 0.25))
-    local preview_width = layout_width - menu_width
-    local outer = Layout({
-        position = "50%",
-        size = {
-            width = layout_width,
-            height = "80%",
         },
-    }, {
-        Layout.Box({
-            Layout.Box(menu, { size = menu_width }),
-            Layout.Box(preview, { size = preview_width }),
-        }, { dir = "row", size = "100%" })
-    })
-
-    return {
-        menu = menu,
-        preview = preview,
-        outer = outer,
     }
-end
-
---- Open a new quickfix
----@param title string The title to give to the quickfix
----@param items Quickfix.item[] list of items to show in the quickfix
-function M.open(title, items)
-    if quickfix then close() end
-    quickfix = {
-        preview_buffers = {},
-        ui = make_ui(title, items),
-    }
-
-    quickfix.ui.outer:mount()
+    return opts
 end
 
 ---@param entires vim.quickfix.entry[]
 ---@return Quickfix.item[] itmes
-function M.conv_setqflist_what(entires)
+---@private
+function M.conv_qlist_entries(entires)
     local items = {}
     for _, entry in ipairs(entires) do
         -- TODO: improve this
@@ -198,35 +185,16 @@ function M.conv_setqflist_what(entires)
 
         ---@type Quickfix.item
         local item = {
-            text = entry.text,
+            text = entry.text:match("^%s*(.-)%s*$"),
             path = entry.filename,
+            line = entry.lnum,
         }
-
-        if entry.lnum then
-            item.pos = {
-                line = entry.lnum,
-                col = entry.col,
-            }
-        end
 
         items[#items+1] = item
 
         ::continue::
     end
     return items
-end
-
----@return vim.lsp.ListOpts
-function M.list_opts()
-    ---@type vim.lsp.ListOpts
-    local opts = {
-        on_list = function(t)
-            local items = M.conv_setqflist_what(t.items)
-            local title = t.title or "[Quickfix]"
-            M.open(title, items)
-        end
-    }
-    return opts
 end
 
 return M
